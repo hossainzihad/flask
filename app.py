230f262fe7e61aa069cc08a538ff9199fad24120
hossain_zihad <hossain.zihad06@gmail.com>
2023-03-08 20:04:21 +0600

    templates

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
-import urllib.parse
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
-@app.route('/cal/<int:num1>/<string:op>/<int:num2>')
+
+@app.route('/cal/<int:num1>/<op>/<int:num2>')
 def calculator(num1, op, num2):
     if op == '+':
         result = num1 + num2
@@ -18,10 +19,18 @@ def calculator(num1, op, num2):
     else:
         return "Invalid operator"
 
-    # Decode URL-encoded operator
-    op = urllib.parse.unquote(op)
-
     return f"{num1} {op} {num2} = {result}"
+
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
